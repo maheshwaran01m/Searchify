d2189d6993e91b1a080c24d1a0fbd39563d969a0
MAHESHWARAN <maheshwaran99m@gmail.com>
2023-05-21 08:11:54 +0530

    created searchify base repo

---
+++ Searchify/Controller/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Searchify
+//
+//  Created by MAHESHWARAN on 21/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureView()
+  }
+  
+  private func configureView() {
+    self.view.backgroundColor = .systemBackground
+    self.title = "Searchify"
+  }
+}
+
