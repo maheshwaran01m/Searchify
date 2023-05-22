f94debc279092052b6178d1596d77607e8cc88c8
MAHESHWARAN <maheshwaran@eam360.com>
2023-05-22 09:01:52 +0530

    SearchParameter  - Created files for searchParameters

---
+++ Searchify/Controller/SearchParameter/SearchParameterCell.swift
@@ -0,0 +1,8 @@
+//
+//  SearchParameterCell.swift
+//  Searchify
+//
+//  Created by MAHESHWARAN on 22/05/23.
+//
+
+import Foundation
---
+++ Searchify/Controller/SearchParameter/SearchParameterView.swift
@@ -0,0 +1,8 @@
+//
+//  SearchParameterView.swift
+//  Searchify
+//
+//  Created by MAHESHWARAN on 22/05/23.
+//
+
+import Foundation
