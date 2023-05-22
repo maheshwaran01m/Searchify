3f32df9be1d137add03f69f0b43dfece8789de45
MAHESHWARAN <maheshwaran99m@gmail.com>
2023-05-22 09:01:52 +0530

    SearchParameter  - Created files for searchParameters

---
+++ Searchify/Controller/SearchParameter/SearchParameterListCell.swift
@@ -0,0 +1,8 @@
+//
+//  SearchParameterListCell.swift
+//  Searchify
+//
+//  Created by MAHESHWARAN on 22/05/23.
+//
+
+import Foundation
---
+++ Searchify/Controller/SearchParameter/SearchParameterVC.swift
@@ -0,0 +1,8 @@
+//
+//  SearchParameterVC.swift
+//  Searchify
+//
+//  Created by MAHESHWARAN on 22/05/23.
+//
+
+import Foundation
