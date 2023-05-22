8a2c13a48fc1e987344a95a4fa42f12e60764ad8
MAHESHWARAN <maheshwaran@eam360.com>
2023-05-22 09:32:28 +0530

    SearchParameterListCell  - Created HeaderView for searchParameters

--- Searchify/Controller/SearchParameter/SearchParameterCell.swift
+++ Searchify/Controller/SearchParameter/SearchParameterCell.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import UIKit
+
+/// ` Left Align the Cells in Collection View `
+/// This Custom UICollectionViewFlowLayout, is used to solve the uneven spacing in collection cell,
+/// it will provide equal padding between each cell in a collection view.
+///
+///    Usage:
+///    let layout = CustomFlowLayout()
+///    collectionView.collectionViewLayout = layout
+///    collectionView.contentInsetAdjustmentBehavior = .always
+///
+/// Source: " https://stackoverflow.com/questions/22539979/
+/// left-align-cells-in-uicollectionview/36016798#36016798 "
+///
+class CustomFlowLayout: UICollectionViewFlowLayout {
+  
+  let cellSpacing: CGFloat = 8.0
+  
+  override func layoutAttributesForElements(
+    in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+      
+      self.minimumLineSpacing = 8.0
+      self.sectionInset = UIEdgeInsets(top: 10, left: 8, bottom: 10, right: 8)
+      
+      let attributes = super.layoutAttributesForElements(in: rect)
+      
+      var leftMargin = sectionInset.left
+      var maxY: CGFloat = -1.0
+      attributes?.forEach { layoutAttribute in
+        if layoutAttribute.frame.origin.y >= maxY {
+          leftMargin = sectionInset.left
+        }
+        layoutAttribute.frame.origin.x = leftMargin
+        leftMargin += layoutAttribute.frame.width + cellSpacing
+        maxY = max(layoutAttribute.frame.maxY, maxY)
+      }
+      return attributes
+    }
+}
+
--- Searchify/Controller/SearchParameter/SearchParameterListCell.swift
+++ Searchify/Controller/SearchParameter/SearchParameterListCell.swift
@@ -6,3 +6,81 @@
 //
 
 import Foundation
+import UIKit
+
+protocol SearchParamsListCellDelegate: AnyObject {
+  func didTapOnClearButton(_ cell: SearchParamsListCell)
+}
+
+class SearchParamsListCell: UICollectionViewCell {
+  
+  weak var delegate: SearchParamsListCellDelegate?
+  
+  let detailLabel: UILabel = {
+    $0.font = UIFont.systemFont(ofSize: 14)
+    $0.textColor = .black
+    $0.textAlignment = .center
+    return $0
+  }(UILabel())
+  
+  lazy var clearButton: UIButton = {
+    let cancelImage = UIImage(systemName: "xmark.circle.fill")?.withTintColor(.gray, renderingMode: .alwaysOriginal)
+    $0.setImage(cancelImage, for: .normal)
+    $0.addTarget(self, action: #selector(clearButtonClicked(_:)), for: .touchUpInside)
+    return $0
+  }(UIButton())
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupView()
+    addAccessibilityLabels()
+  }
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    detailLabel.text = nil
+  }
+  
+  // MARK: - Appearance
+  
+  private func setupView() {
+    contentView.backgroundColor = .systemBackground
+    contentView.layer.borderColor = UIColor.gray.cgColor
+    contentView.layer.borderWidth = 0.5
+    contentView.layer.cornerRadius = 6.0
+    contentView.layer.masksToBounds = true
+    contentView.addSubview(detailLabel)
+    contentView.addSubview(clearButton)
+    configureConstraints()
+  }
+  
+  func configureView(using value: String) {
+    detailLabel.text = value
+  }
+  
+  private func configureConstraints() {
+    NSLayoutConstraint.activate([
+      clearButton.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -8),
+      clearButton.widthAnchor.constraint(equalToConstant: 30),
+      clearButton.heightAnchor.constraint(equalToConstant: 30),
+      
+      detailLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
+      detailLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+      detailLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 8),
+      detailLabel.rightAnchor.constraint(equalTo: clearButton.leftAnchor, constant: -2),
+      detailLabel.heightAnchor.constraint(equalToConstant: 30)
+    ])
+  }
+  
+  @objc private func clearButtonClicked(_ sender: UIButton) {
+    delegate?.didTapOnClearButton(self)
+  }
+  
+  private func addAccessibilityLabels() {
+    detailLabel.accessibilityIdentifier = "detailLabel"
+    clearButton.accessibilityIdentifier = "clearButon"
+  }
+}
